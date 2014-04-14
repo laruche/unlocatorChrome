4f4d1654a663edc999bad5bdf94d7b5396efc370
Laruche <loic@op1.com>
2014-04-14 17:19:20 +0100

    add boucle + changement cert

--- unpacked/js/background.js
+++ unpacked/js/background.js
@@ -1,24 +1,36 @@
 /// infos
 
 var updateIP = function() {
+  console.log('updateIP');
   checkIP(function(ip) {
     getAPIKEY(function(link) {      
       jQuery.ajax({
           url: link,        
         }).error(function(data) {
           //error
+          chrome.browserAction.setBadgeBackgroundColor({color:[254, 51, 0, 255]}); 
+          chrome.browserAction.setBadgeText({text: '?'});
+          callmelater();
         }).done(function(data) { 
-
+          chrome.storage.local.set({'MSG_unlocator': data}, function() {});
           switch(data) {
-            case 'Missing or Invalid API Key':
+            case 'Missing or Invalid API Key':              
               chrome.browserAction.setBadgeBackgroundColor({color:[254, 51, 0, 255]}); 
               chrome.browserAction.setBadgeText({text: '?'});
+              chrome.browserAction.setTitle({title: 'Missing or Invalid API Key'});
               break;
 
+            case 'Your Subscription Has Expired ':
+              chrome.browserAction.setBadgeBackgroundColor({color:[254, 51, 0, 255]}); 
+              chrome.browserAction.setBadgeText({text: 'x'});
+              chrome.browserAction.setTitle({title: 'Your Subscription Has Expired'});
+              break;  
+
             case "IP "+ip+" exists":
-            case "IP "+ip+" added":              
+            case "IP "+ip+" added":          
               chrome.browserAction.setBadgeBackgroundColor({color:[7, 149, 0, 255]}); 
               chrome.browserAction.setBadgeText({text: '✓'});
+              chrome.browserAction.setTitle({title: 'Let\'s Have Fun !'});
               break;
 
             default:               
@@ -33,9 +45,17 @@ var updateIP = function() {
   });
 }
 
+var callmelater = function() {
+  console.log("call in 5'");
+  setTimeout(function(){
+    updateIP();
+    console.log("called in 5'");
+  },5000);
+}
+
 var getAPIKEY = function(cb) {
   var searchValue = 'APIKey_unloc';
-  chrome.storage.sync.get(searchValue,function(object){
+  chrome.storage.local.get(searchValue,function(object){
     if(typeof(object[searchValue]) == 'undefined')
     {
       cb(null);
@@ -48,7 +68,7 @@ var getAPIKEY = function(cb) {
 
 var checkIP = function(cb) {
   var searchValue = 'IP_unlocator';
-  chrome.storage.sync.get(searchValue,function(object){
+  chrome.storage.local.get(searchValue,function(object){
     if(typeof(object[searchValue]) == 'undefined')
     {
       cb(null);
@@ -65,10 +85,23 @@ var getIP = function() {
       type: "GET",
       url: "https://jsonip.appspot.com/",
       dataType : 'json'
+    }).error(function(data) {
+      $.ajax({
+        type: "GET",
+        url: "http://ipinfo.io/ip",
+        dataType : 'TXT'
+      }).error(function(data) {
+        callmelater();
+      }).done(function(data) {            
+        if(ip != ip) {
+          updateIP();
+          chrome.storage.local.set({'IP_unlocator': data.ip}, function() {});
+        }      
+      });
     }).done(function(data) {            
       if(ip != data.ip) {
         updateIP();
-        chrome.storage.sync.set({'IP_unlocator': data.ip}, function() {});
+        chrome.storage.local.set({'IP_unlocator': data.ip}, function() {});
       }      
     });
   })  
@@ -77,17 +110,50 @@ var getIP = function() {
 chrome.browserAction.setBadgeBackgroundColor({color:[254, 51, 0, 255]}); 
 chrome.browserAction.setBadgeText({text: '?'});
 
-chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {  
-  getIP();
-});
 
-chrome.tabs.onSelectionChanged.addListener(function(tabId) {
-  getIP();
-});
+var createRefresh = function() {
+  var searchValueRefresh = 'refresh';
+  chrome.storage.local.get(searchValueRefresh,function(object){
+    if(typeof(object[searchValueRefresh]) != 'undefined')
+    {
+      UnlRefresher(object[searchValueRefresh])
+    } else {
+      chrome.storage.local.set({'refresh' : '30000'}, function() {
+          UnlRefresher();
+      });
+    }
+  });
+}
+var makeAnUpdate = '',
+    call= 0
 
-chrome.tabs.onCreated.addListener(function(tabId, changeInfo, tab) {         
-  getIP();
+var UnlRefresher = function(TimeR) {
+  var searchValueRefresh = 'refresh';
+  chrome.storage.local.get(searchValueRefresh,function(object){
+    console.log(object[searchValueRefresh]);
+    if(typeof(object[searchValueRefresh]) != 'undefined')
+    {
+      var timer = object[searchValueRefresh];
+      makeAnUpdate = setInterval(function() {
+        getIP();
+        call++;
+        chrome.browserAction.setBadgeText({text: ''+call});
+      }, parseInt(timer));
+    }
+  });
+  
+}
+
+
+chrome.storage.onChanged.addListener(function(changes, namespace) {
+  for (key in changes) {
+    if(key == 'refresh')
+      clearInterval(makeAnUpdate);
+      createRefresh();
+    if(key == 'APIKey_unloc' || key == 'IP_unlocator')
+      updateIP();
+  }
 });
 
 getIP();
-updateIP();
+createRefresh();
--- unpacked/js/login.js
+++ unpacked/js/login.js
@@ -1,19 +1,49 @@
 $( document ).ready(function() {
   var searchValue = 'APIKey_unloc';
   //check APIKEY
-  chrome.storage.sync.get(searchValue,function(object){
+  chrome.storage.local.get(searchValue,function(object){
     if(typeof(object[searchValue]) != 'undefined')
     {
       $('#APIKey').val(object[searchValue]);      
+      var bg = chrome.extension.getBackgroundPage();
+      bg.updateIP();
+    }
+  });
+
+  chrome.storage.local.get('MSG_unlocator',function(object){
+
+    if(typeof(object['MSG_unlocator']) != 'undefined')
+    {
+      var message = object['MSG_unlocator'];
+      if(message == "Your Subscription Has Expired ") {      
+        $('#messageAPI').html('<div class="alert alert-danger alert-dismissable">Your Subscription Has Expired, <a target="_blank" href="http://unlocator.com/account/aff/go/a41FRdeZON3XIS37">Upgrade to paid version</a></div>');          
+        $('#messageAPIHelp').hide();
+        $('#messageAccount').hide();
+      } else if(message == 'Missing or Invalid API Key') {
+        $('#messageAPI').html('<div class="alert alert-warning alert-dismissable">Please add you API Key on Extension</div>');          
+      } else {
+        $('#messageAPIHelp').hide();
+        $('#messageAccount').hide();
+      }
+    }
+  });
+
+  chrome.storage.local.get('refresh',function(object){
+    if(typeof(object['refresh']) != 'undefined')
+    {
+      $('#APITime').val(object['refresh']/60/1000);            
     }
   });
 
   $( "#loginForm" ).submit(function( event ) {
     event.preventDefault();    
-    chrome.storage.sync.set({"APIKey_unloc": $('#APIKey').val()}, function() {
-      var bg = chrome.extension.getBackgroundPage();
-      bg.updateIP();
-    });
+    chrome.storage.local.set({"APIKey_unloc": $('#APIKey').val()}, function() {});
+    
+  });
+
+  $( "#refreshForm" ).submit(function( event ) {
+    event.preventDefault();    
+    chrome.storage.local.set({"refresh": $('#APITime').val()*60*1000}, function() {});
     
   });
 });
